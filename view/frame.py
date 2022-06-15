26432909afa4e44cc94487088f75e30d5a6b70d6
nb-crypto <n.baudis@hotmail.de>
2022-06-15 13:30:00 +0200

    hintergrundfarbe

--- view/frame.py
+++ view/frame.py
@@ -1,3 +1,4 @@
+import tkinter
 import tkinter.messagebox
 from tkinter import *
 
@@ -9,11 +10,12 @@ class Frame(Tk):
 
     def __init__(self):
         super().__init__()
-
+        self.__background = "#00CED1"
         self.columnconfigure(0, weight=1)
 
     def idk_yet(self):
         self.title("WakeApp")
+        self.configure(bg=self.__background)
         self.__init_mainframe()
         self.mainloop()
 
@@ -23,32 +25,42 @@ class Frame(Tk):
         self.__init_button()
 
     def __init_label(self):
-        label_title = Label(master=self, text="WakeApp", font=("times", 20, "bold", "italic"), fg="#00CED1")
+        sticky = tkinter.W
+        padx = 5
+        label_title = Label(master=self, text="WakeApp", font=("times", 20, "bold", "italic"),
+                            fg="white", bg=self.__background, pady=20)
         label_title.grid(row=0, column=0, columnspan=4)
-        label_arrival_time = Label(master=self, text="Bitte geben sie die Ankunfstzeit ein (hh:mm)")
-        label_arrival_time.grid(row=1, column=0)
-        label_duration = Label(master=self, text="Bitte geben sie die Dauer des Fahrtwegs ein (hh:mm)")
-        label_duration.grid(row=2, column=0)
-        label_prep_time = Label(master=self, text="Bitte geben sie die Dauer ihrer morgendlicher Routine ein (hh:mm)")
-        label_prep_time.grid(row=3, column=0)
-        label_delay = Label(master=self, text="Bitte geben sie die Dauer der Verzögerung ein (hh:mm)")
-        label_delay.grid(row=4, column=0)
+        label_arrival_time = Label(master=self, text="Bitte geben sie die Ankunfstzeit ein (hh:mm)",
+                                   bg=self.__background)
+        label_arrival_time.grid(row=1, column=0, sticky=sticky, padx=padx)
+        label_duration = Label(master=self, text="Bitte geben sie die Dauer des Fahrtwegs ein (hh:mm)",
+                               bg=self.__background)
+        label_duration.grid(row=2, column=0, sticky=sticky, padx=padx)
+        label_prep_time = Label(master=self, text="Bitte geben sie die Dauer ihrer morgendlicher Routine ein (hh:mm)",
+                                bg=self.__background)
+        label_prep_time.grid(row=3, column=0, sticky=sticky, padx=padx)
+        label_delay = Label(master=self, text="Bitte geben sie die Dauer der Verzögerung ein (hh:mm)",
+                            bg=self.__background)
+        label_delay.grid(row=4, column=0, sticky=sticky, padx=padx)
 
     def __init_entries(self):
+        pady = 5
+        padx = 10
         self.__entry_arrival_time = Entry(master=self, bg="white")
-        self.__entry_arrival_time.grid(row=1, column=1)
+        self.__entry_arrival_time.grid(row=1, column=1, pady=pady, padx=padx)
         self.__entry_duration = Entry(master=self, bg="white")
-        self.__entry_duration.grid(row=2, column=1)
+        self.__entry_duration.grid(row=2, column=1, pady=pady, padx=padx)
         self.__entry_prep_time = Entry(master=self, bg="white")
-        self.__entry_prep_time.grid(row=3, column=1)
+        self.__entry_prep_time.grid(row=3, column=1, pady=pady, padx=padx)
         self.__entry_delay = Entry(master=self, bg="white")
-        self.__entry_delay.grid(row=4, column=1)
+        self.__entry_delay.grid(row=4, column=1, pady=pady, padx=padx)
 
     def __init_button(self):
+        pady = 30
         clear_button = Button(master=self, text="Eingaben löschen", command=self.__clear_entries)
-        clear_button.grid(row=5, column=0)
+        clear_button.grid(row=5, column=0, pady=pady)
         calculate_button = Button(master=self, text="Starte Berechnung", command=self.__calculate)
-        calculate_button.grid(row=5, column=2)
+        calculate_button.grid(row=5, column=1, pady=pady, sticky=tkinter.E, padx=10)
 
     def __clear_entries(self):
         self.__entry_prep_time.delete(0, END)
@@ -64,7 +76,7 @@ class Frame(Tk):
                         self.__entry_delay.get())
             get_up_time = WakeController.computation(time)
             get_up_msg = "Sie müssen um " + str(get_up_time) + " aufstehen!"
-            tkinter.messagebox.showinfo(title="Aufstehzeit", message=get_up_msg)
+            tkinter.messagebox.showinfo(title="Aufstehzeit", message=get_up_msg, bg=self.__background)
             self.__clear_entries()
         except ValueError:
             error_msg = "Mindestens eine Eingabe ist fehlerhaft und entspricht nicht dem vorgegebenen Format (hh:mm)!" \
